1a5294099cad3e546d4fdd2c36747a4747c9f96e
Chinatsu <chinatsu.k@hotmail.com>
2017-09-06 23:13:15 -0700

    footer mobile and desktop

--- themes/vatjss/footer.php
+++ themes/vatjss/footer.php
@@ -10,8 +10,15 @@
 			</div><!-- #content -->
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
+				<div class="vatjss-footer-icons">
+					<a href="https://www.facebook.com/VATJSS/"><img class="facebook-icon vatjss-socialmedia-icon" src=<?php echo get_stylesheet_directory_uri(); ?>/images/facebook-logo-1.svg alt="facebook icon"/></a>
+					<a href="https://twitter.com/vatjss?lang=en"><img class="twitter-icon vatjss-socialmedia-icon" src=<?php echo get_stylesheet_directory_uri(); ?>/images/twitter-sign.svg alt="twitter icon"/></a>
+					<a href="https://www.instagram.com/vatjss/"><img class="instagram-icon vatjss-socialmedia-icon" src=<?php echo get_stylesheet_directory_uri(); ?>/images/instagram-symbol.svg alt="instagram icon"/></a>
+				</div>
+
 				<div class="site-info">
-				<p class="vatjss-footer">&copy; 2017 <span class="vatjss-text-uppercase">vatjss</span>(Vancouver Aboriginal Transformative Services). All Rights Reserved.</p>
+					<p class="vatjss-footer-mobile">&copy; 2017 <span class="vatjss-text-uppercase">vatjss.</span> All Rights Reserved.<p>
+					<p class="vatjss-footer-web">&copy; 2017 <span class="vatjss-text-uppercase">vatjss</span> (Vancouver Aboriginal Transformative Justice Services Society). All Rights Reserved.</p>
 				</div><!-- .site-info -->
 			</footer><!-- #colophon -->
 		</div><!-- #page -->
--- themes/vatjss/header.php
+++ themes/vatjss/header.php
@@ -12,6 +12,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="profile" href="http://gmpg.org/xfn/11">
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+		<script src="https://use.fontawesome.com/e28c7aff88.js"></script>
 
 	<?php wp_head(); ?>
 	</head>
