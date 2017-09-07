da531376afc7293bb666ad4f1fabd91468c6e25f
bsoetarto7 <bsoetarto7@gmail.com>
2017-09-06 17:28:09 -0700

    Style front page more

--- themes/vatjss/functions.php
+++ themes/vatjss/functions.php
@@ -83,9 +83,9 @@ add_filter( 'stylesheet_uri', 'vatjss_minified_css', 10, 2 );
  * Enqueue scripts and styles.
  */
 function vatjss_scripts() {
-	wp_enqueue_style( 'red-starter-style', get_stylesheet_uri() );
+	wp_enqueue_style( 'vatjss-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'red-starter-skip-link-focus-fix', get_template_directory_uri() . '/build/js/skip-link-focus-fix.min.js', array(), '20130115', true );
+	wp_enqueue_script( 'vatjss-skip-link-focus-fix', get_template_directory_uri() . '/build/js/skip-link-focus-fix.min.js', array(), '20130115', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
