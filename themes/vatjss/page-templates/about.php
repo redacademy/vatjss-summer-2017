604df1fd25370aac6c5d84a63e22a822adc4f7bb
Chinatsu <chinatsu.k@hotmail.com>
2017-09-08 15:54:48 -0700

    About page do's: pull images, style our story and do widget

--- themes/vatjss/page-templates/about.php
+++ themes/vatjss/page-templates/about.php
@@ -7,73 +7,42 @@
 
 get_header(); ?>
 
-<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-				<header class="entry-header">
-					<div class="vatjss-custom-hero">
-            <h2 class="vatjss-about">Crime is best viewed as a violate of people and relationships. 
-              Conflict that emerges is an opportunity for positive 
-              <span class="vatjss-text-uppercase">transformation</span> 
-              for the victim, offender, and the community.</h2>
-					</div>
-				</header>
-				<div class="container about-content">
-							<h2 class="story-header">Our Story</h2>
-							<?php echo CFS()->get( ' ' ); ?>	
-							<h2 class="story-header">Our Team</h2>
-							<?php echo CFS()->get( ' ' ); ?>
-				</div>
-		</main><!-- #main -->
+<div id="primary" class="about-content">
+	<main id="main" class="site-main" role="main">
+		<header class="entry-header">
+			<div class="vatjss-custom-hero">
+        <!--get hero banner-->
+        <h2 class="vatjss-about">Crime is best viewed as a violate of people and relationships. 
+          Conflict that emerges is an opportunity for positive 
+          <span class="vatjss-text-uppercase">transformation</span> 
+          for the victim, offender, and the community.</h2>
+			</div>
+		</header>
+		<section class="container vatjss-about-content">
+			<div class="vatjss-about-category">
+				<h2 class="vatjss-story-header">Our Mission</h2>
+					<?php echo CFS()->get( 'about_our_mission' ); ?>	
+			</div>
+			<div class="vatjss-our-vision vatjss-about-category">
+				<h2 class="vatjss-story-header">Our Vision</h2>
+      	  <?php echo CFS()->get( 'our_vision' ); ?>
+			</div>
+			<div class="vatjss-about-category">
+      	<h2 class="vatjss-story-header">Our Story</h2>
+					<?php echo CFS()->get( 'about_our_story' ); ?>	
+			</div>
+			<div class="vatjss-about-category">
+				<h2 class="vatjss-story-header">Funding</h2>
+					<?php echo CFS()->get( 'about_funding' ); ?>
+			</div>
+    </section>
+    <section class="container vatjss-staff-content">
+      <!--get staff photo-->
+      <h2 class="vatjss-our-staff">Our Staff</h2>
+        <?php echo CFS()->get( 'our_staff' ); ?>
+    </section>
+	</main><!-- #main -->
 	</div><!-- #primary -->
 
-
-
-<!-- <div id="primary" class="content-area">
-  <main id="main" class="site-main" role="main">
-    <section class="vatjss-home-hero">
-
-    </section>
-    <section class="vatjss-home-banner vatjss-vertical-align-center">
-      <h2 class="vatjss-text-uppercase vatjss-text-center">strengthening resiliency within the aboriginal community</h2>
-    </section>
-    <section class="vatjss-container-fluid">
-      <?php
-        $service_args = array( 'taxonomy' => 'services-type');
-        $service_types = get_terms( $service_args );
-      ?>
-      <?php foreach ( $service_types as $service_type ) : setup_postdata( $service_type ); ?>
-        <div>
-          <p><?php echo $service_type->description ?></p>
-        </div>
-      <?php endforeach; wp_reset_postdata(); ?>
-      <?php
-        $resource_args = array( 'taxonomy' => 'resources-type');
-        $resource_types = get_terms( $resource_args );
-      ?>
-
-<div class="main-carousel" data-flickity='{ "cellAlign": "left", "contain": true }'>
-  <element class='carousel-cell'><p>words</p></element>
-  <element class='carousel-cell'><p>have</p></element>
-  <element class='carousel-cell'><p>been</p></element>
-  <element class='carousel-cell'><p>worded</p></element>
-  <element class='carousel-cell'><p>in</p></element>
-  <element class='carousel-cell'><p>a</p></element>
-  <element class='carousel-cell'><p>very</p></element>
-  <element class='carousel-cell'><p>wordy</p></element>
-  <element class='carousel-cell'><p>way</p></element>
-</div>
-
-      <?php foreach ( $resource_types as $resource_type ) : setup_postdata( $service_type ); ?>
-        <div>
-          <p><?php echo $resource_type->description ?></p>
-        </div>
-      <?php endforeach; wp_reset_postdata(); ?>
-    </section>
-    <section class="vatjss-home-mobile-subscribe-banner vatjss-vertical-align-center">
-      <button>Subscribe</button>
-    </section>
-  </main>
-</div> -->
-
-
+<?php get_sidebar(); ?>
 <?php get_footer(); ?>
