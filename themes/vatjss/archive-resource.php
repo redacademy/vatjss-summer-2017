5d48f70e04a44df614152366658d744f58273383
Camden_Shaw <camden.shaw@gmail.com>
2017-09-10 00:11:29 -0700

    added divs and classes for styling and js functionality to resources page

--- themes/vatjss/archive-resource.php
+++ themes/vatjss/archive-resource.php
@@ -18,27 +18,32 @@ get_header(); ?>
             <?php
               $terms = get_terms( 'resources-type' );
               $args1 = array( 'child_of' => 33, 'parent' => 33 );
-              $term_children = get_terms( 'resources-type', $args1 );
-              foreach ( $term_children as $term_child ) : setup_postdata( $term_child );
-                $args2 = array( 
-                  'numberposts' =>  -1,
-                  'post_type' => 'resource',
-                  'tax_query' => array(
-                    array(
-                      'taxonomy' => 'resources-type',
-                      'field'    => 'slug',
-                      'terms'    => $term_child,
-                    ),
-                  )
-                );
-                $resource_posts = get_posts( $args2 );
+              $term1_children = get_terms( 'resources-type', $args1 );
+              foreach ( $term1_children as $term1_child ) : setup_postdata( $term1_child );
+                echo '<h4>' . $term1_child->name . '</h4>';
+                $args2 = array( 'child_of' => $term1_child->term_id, 'parent' => $term1_child->term_id );
+                $term2_children = get_terms( 'resources-type', $args2 );
+                foreach ( $term2_children as $term2_child ) : setup_postdata( $term2_child );
+                  $args3 = array( 
+                    'numberposts' =>  -1,
+                    'post_type' => 'resource',
+                    'tax_query' => array(
+                      array(
+                        'taxonomy' => 'resources-type',
+                        'field'    => 'slug',
+                        'terms'    => $term2_child,
+                      ),
+                    )
+                  );
+                  $resource_posts = get_posts( $args3 );
             ?>
-                <h4><?php echo $term_child->name ?></h4>
-                <ul>
-                  <?php foreach ( $resource_posts as $resource ) : setup_postdata( $resource ); ?>
-                    <li><a href="<?php echo $resource->link_url ?>" target="_blank"><?php echo $resource->list_item ?></a></li>
-                  <?php endforeach; wp_reset_postdata(); ?>
-                </ul>
+                  <h5><?php echo $term2_child->name ?></h5>
+                  <ul>
+                    <?php foreach ( $resource_posts as $resource ) : setup_postdata( $resource ); ?>
+                      <li><a href="<?php echo $resource->link_url ?>" target="_blank"><?php echo $resource->list_item ?></a></li>
+                    <?php endforeach; wp_reset_postdata(); ?>
+                  </ul>
+                <?php endforeach; wp_reset_postdata() ?>
               <?php endforeach; wp_reset_postdata() ?>
           </div>
         </section>
@@ -47,27 +52,32 @@ get_header(); ?>
           <div class="panel">
             <?php
                 $args1 = array( 'child_of' => 5, 'parent' => 5 );
-                $term_children = get_terms( 'resources-type', $args1 );
-                foreach ( $term_children as $term_child ) : setup_postdata( $term_child );
-                  $args2 = array( 
-                    'numberposts' =>  -1,
-                    'post_type' => 'resource',
-                    'tax_query' => array(
-                      array(
-                        'taxonomy'  => 'resources-type',
-                        'field'     => 'slug',
-                        'terms'     => $term_child,
-                      ),
-                    )
-                  );
-                  $resource_posts = get_posts( $args2 );
+                $term1_children = get_terms( 'resources-type', $args1 );
+                foreach ( $term1_children as $term1_child ) : setup_postdata( $term1_child );
+                  echo '<h4>' . $term1_child->name . '</h4>';
+                  $args2 = array( 'child_of' => $term1_child->term_id, 'parent' => $term1_child->term_id );
+                  $term2_children = get_terms( 'resources-type', $args2 );
+                  foreach ( $term2_children as $term2_child ) : setup_postdata( $term2_child );
+                    $args3 = array( 
+                      'numberposts' =>  -1,
+                      'post_type' => 'resource',
+                      'tax_query' => array(
+                        array(
+                          'taxonomy'  => 'resources-type',
+                          'field'     => 'slug',
+                          'terms'     => $term2_child,
+                        ),
+                      )
+                    );
+                    $resource_posts = get_posts( $args3 );
             ?>
-                  <h4><?php echo $term_child->name ?></h4>
-                  <ul>
-                    <?php foreach ( $resource_posts as $resource ) : setup_postdata( $resource ); ?>
-                      <li><a href="<?php echo $resources->link_url ?>" target="blank"><?php echo $resource->list_item ?></a></li>
-                    <?php endforeach; wp_reset_postdata(); ?>
-                  </ul>
+                    <h5><?php echo $term2_child->name ?></h5>
+                    <ul>
+                      <?php foreach ( $resource_posts as $resource ) : setup_postdata( $resource ); ?>
+                        <li><a href="<?php echo $resources->link_url ?>" target="blank"><?php echo $resource->list_item ?></a></li>
+                      <?php endforeach; wp_reset_postdata(); ?>
+                    </ul>
+                  <?php endforeach; wp_reset_postdata(); ?>
                 <?php endforeach; wp_reset_postdata(); ?>
           </div>
         </section>
@@ -76,27 +86,34 @@ get_header(); ?>
           <div class="panel">
           <?php
               $args1 = array( 'child_of' => 7, 'parent' => 7 );
-              $term_children = get_terms( 'resources-type', $args1 );
-              foreach ( $term_children as $term_child ) : setup_postdata( $term_child );
-                $args2 = array( 
-                  'numberposts' =>  -1,
-                  'post_type' => 'resource',
-                  'tax_query' => array(
-                    array(
-                      'taxonomy' => 'resources-type',
-                      'field'    => 'slug',
-                      'terms'    => $term_child,
-                    ),
-                  )
-                );
-                $resource_posts = get_posts( $args2 );
+              $term1_children = get_terms( 'resources-type', $args1 );
+              echo '<div class="resources-accordion">';
+              foreach ( $term1_children as $term1_child ) : setup_postdata( $term1_child );
+                echo '<div class=resource-accordion-title-' . $term1_child->term_id . '"><h4>' . $term1_child->name . '</h4></div><div class=resource-accordion-section-' . $term1_child->term_id . '>';
+                $args2 = array( 'child_of' => $term1_child->term_id, 'parent' => $term1_child->term_id );
+                $term2_children = get_terms( 'resources-type', $args2 );
+                foreach ( $term2_children as $term2_child ) : setup_postdata( $term2_child );
+                  $args3 = array( 
+                    'numberposts' =>  -1,
+                    'post_type' => 'resource',
+                    'tax_query' => array(
+                      array(
+                        'taxonomy' => 'resources-type',
+                        'field'    => 'slug',
+                        'terms'    => $term2_child,
+                      ),
+                    )
+                  );
+                  $resource_posts = get_posts( $args3 );
           ?>
-                <h4><?php echo $term_child->name ?></h4>
-                <ul>
-                  <?php foreach ( $resource_posts as $resource ) : setup_postdata( $resource ); ?>
-                  <li><a href="<?php echo $resources->link_url ?>" target="_blank"><?php echo $resource->list_item ?></a></li>
-                  <?php endforeach; wp_reset_postdata(); ?>
-                </ul>
+                  <h5><?php echo $term2_child->name ?></h5>
+                  <ul>
+                    <?php foreach ( $resource_posts as $resource ) : setup_postdata( $resource ); ?>
+                    <li><a href="<?php echo $resources->link_url ?>" target="_blank"><?php echo $resource->list_item ?></a></li>
+                    <?php endforeach; wp_reset_postdata(); ?>
+                  </ul>
+                <?php endforeach; wp_reset_postdata(); ?>
+                </div> 
               <?php endforeach; wp_reset_postdata(); ?>
           </div>
         </section>
