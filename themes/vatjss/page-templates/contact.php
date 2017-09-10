4fda7ddd67095ec1c1e6d32fd3ca1c7a1db0020a
Camden_Shaw <camden.shaw@gmail.com>
2017-09-10 15:59:31 -0700

    basic functionality for contact page

--- themes/vatjss/page-templates/contact.php
+++ themes/vatjss/page-templates/contact.php
@@ -5,9 +5,16 @@
  * @package VATJSS_Theme
  */
 
-get_header();
+get_header(); ?>
+  <div id="primary" class="content-area">
+    <main id="main" class="site-main" role="main">
 
-get_sidebar();
+      <?php while ( have_posts() ) : the_post(); ?>
 
-get_footer();
-?>
+        <?php get_template_part( 'template-parts/content', 'page' ); ?>
+
+      <?php endwhile; // End of the loop. ?>
+
+    </main><!-- #main -->
+  </div><!-- #primary -->
+<?php get_footer(); ?>
