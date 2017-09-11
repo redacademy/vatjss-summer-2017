c1dd4cffba301bf1dbc2da2d580d524b15c30a89
Ayesha Kanani <isha_21@yahoo.com>
2017-09-11 14:13:11 -0700

    added volunteer info to contact page

--- themes/vatjss/page-templates/contact.php
+++ themes/vatjss/page-templates/contact.php
@@ -37,7 +37,35 @@ get_header(); ?>
       <div class="vatjss-volunteer-hide"> <h3> Volunteer Opportunities </h3> </div>
       <div class="vatjss-contact-form-show"> <?php echo CFS()->get( 'contact_form_shortcode' ); ?> </div>
       <div class="vatjss-staff-contact-show"> <!--staff posts--> </div>
-      <div class=""> <!--Volunteer Opportunities--> </div>
+      <div class="volunteer-container">
+        <div class=volunteer-title">
+             <h3>VATJSS is seeking dedicated Community Council Members</h3>
+             <h3> Elders strongly encourage to apply °<span class="no">no</span> experience necessary</h3>
+        </div>
+      <div class="volunteer-requirements">
+          <h4 class="volunteer-requirements-title">Volunteer Requirements</h4>      
+            <ul>              
+              <li>Self-identify as aboriginal  persons 18 and over (Status, Non-status, Inuit, Metis)</li>
+              <li>Willing to commit 3–5 hours a month</li>
+              <li>Commitment to personal and community wellness</li>
+              <li> Complete a Police Information Check (PIC)</li>
+            </ul>
+      </div>
+      <div class ="volunteer-benefits-container">
+          <h4 class="volunteer-benefits-title">Volunteer Benefits</h4>
+            <ul>
+              <li>Provide support to community members using traidtional indigenous healing approaches</li>
+              <li>Offer guidance, wisdom, and culture</li>
+              <li> Provide input for individual healing plans</li>
+              <li>Looks great on your resume</li>
+            </ul>
+       </div>
+      <div class="volunteer-contact">
+        <p>If you are committed to empowering our Urban Aboriginal Community through participation in Traditional Aboriginal Justice Process, please contact:</p>
+        <p>Victoria Grosse</p>
+        <p>(604) 251 – 7200 ext. 175</p>
+        <p>circlecoordinator@vatjss.com</p>
+      </div>
     </div>
   </element>
 
