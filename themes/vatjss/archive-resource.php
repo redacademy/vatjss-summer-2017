0e1a5bc3d205261a17d76b4b259ee2a0b149b9d3
Camden_Shaw <camden.shaw@gmail.com>
2017-09-11 14:30:04 -0700

    modified styling in resource page.  fixed logged in navbar

--- themes/vatjss/archive-resource.php
+++ themes/vatjss/archive-resource.php
@@ -9,19 +9,24 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
+	<div id="primary-resources" class="resources-content-area">
 		<main id="main" class="site-main" role="main">
+      <div class="vatjss-mobile-resource-toggle-container">
+        <div class="housing-toggle resource-toggle"><h3>Housing Resources</h3></div>
+        <div class="justice-toggle resource-toggle"><h3>Justice Resources</h3></div>
+        <div class="community-toggle resource-toggle"><h3>Community Resources</h3></div>
+      </div>
       <div class="vatjss-mobile-accordian">
-        <div class="housing-toggle"><h3>Housing Resources</h3></div>
-        <section class="housing-resource-list">
+        <div class="housing-toggle resource-toggle"><h3>Housing Resources</h3></div>
+        <section class="housing-resource-list resource-list-page">
           <?php get_template_part('template-parts/resources', 'housing'); ?>
         </section>
-        <div class="justice-toggle"><h3>Justice Resources</h3></div>
-        <section class="justice-resource-list">
+        <div class="justice-toggle resource-toggle"><h3>Justice Resources</h3></div>
+        <section class="justice-resource-list resource-list-page">
           <?php get_template_part('template-parts/resources', 'justice'); ?>
         </section>
-        <div class="community-toggle"><h3>Community Resources</h3></div>
-        <section class="community-resource-list">
+        <div class="community-toggle resource-toggle"><h3>Community Resources</h3></div>
+        <section class="community-resource-list resource-list-page">
           <?php get_template_part('template-parts/resources', 'community'); ?>
         </section>
       </div>
