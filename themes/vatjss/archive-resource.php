5080621f5131a0a27dcfce974e6d344baf877b5b
Ayesha Kanani <isha_21@yahoo.com>
2017-09-11 11:24:39 -0700

    added thumbnail image

--- themes/vatjss/archive-resource.php
+++ themes/vatjss/archive-resource.php
@@ -11,7 +11,7 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-      <div class="vatjss-mobile-accordian vatjss-hidden-desktop">
+      <div class="vatjss-mobile-accordian">
         <div class="housing-toggle"><h3>Housing Resources</h3></div>
         <section class="housing-resource-list">
           <?php get_template_part('template-parts/resources', 'housing'); ?>
