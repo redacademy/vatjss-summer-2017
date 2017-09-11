1064562c777bb20e1b4074d0c7d9858c2e9060c3
bsoetarto7 <bsoetarto7@gmail.com>
2017-09-11 12:18:19 -0700

    Added community partners in the calendar events page

--- themes/vatjss/page-templates/events.php
+++ themes/vatjss/page-templates/events.php
@@ -9,8 +9,8 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-			<?php while ( have_posts() ) : the_post(); ?>
       <article id="post-<?php the_ID(); ?>">
+        <?php while ( have_posts() ) : the_post(); ?>
         <section class="vatjss-calendar-events">
           <div class="vatjss-container">
             <div class="vatjss-flex-container-no-wrap">
@@ -18,36 +18,65 @@ get_header(); ?>
               <div class="vatjss-flex-item-desktop-40 vatjss-hidden-mobile">
                 <div class="vatjss-calendar-list-events">
                 <?php 
-                  $events = tribe_get_events( array(
-                    'posts_per_page' => 5,
-                  ) );
-                  if (empty($events)) {
-                    echo "<p class='vatjss-text-center'>There are no upcoming Events</p>";
-                  }else{
-                    foreach ( $events as $event ) {
-                      echo "<div class='vatjss-calendar-list-event'>";
-                      echo "<p class='vatjss-calendar-list-event-date'>". date_format(date_create($event->EventStartDate), 'l, F j, Y') ."</p>";
-                      echo "<h3 class='vatjss-calendar-list-event-title'>". $event->post_title ."</h3>";
-                      echo "<p class='vatjss-calendar-list-event-description'>".$event->post_content."</p>";
-                      echo "</div>";
-                    }
-                  }
+                  #$events = tribe_get_events( array(
+                  #  'posts_per_page' => 5,
+                  #) );
+                  #if (empty($events)) {
+                  #  echo "<p class='vatjss-text-center'>There are no upcoming Events</p>";
+                  #}else{
+                  #  foreach ( $events as $event ) {
+                  #    echo "<div class='vatjss-calendar-list-event'>";
+                  #    echo "<p class='vatjss-calendar-list-event-date'>". date_format(date_create($event->EventStartDate), 'l, F j, Y') ."</p>";
+                  #    echo "<h3 class='vatjss-calendar-list-event-title'>". $event->post_title ."</h3>";
+                  #    echo "<p class='vatjss-calendar-list-event-description'>".$event->post_content."</p>";
+                  #    echo "</div>";
+                  #  }
+                  #}
                 ?>
               </div>
             </div>
           </div>
         </section>
+        <?php endwhile; // End of the loop. ?>
         <section class="vatjss-community-partner-logo-banner">
           <div class="vatjss-container">
             <h2 class="vatjss-text-center">To see what else is going on in your community, check out these other organizations</h2>
           </div>
         </section>
         <section class="vatjss-community-partner-logo-list">
-
+          <div class="vatjss-container">
+              <?php
+                $args = array( 
+                  'numberposts' =>  -1,
+                  'post_type' => 'community_partner'
+                );
+                $partner_posts = get_posts( $args );
+              ?>
+            <div class="vatjss-flex-container-no-wrap vatjss-hidden-mobile">
+              <?php foreach ( $partner_posts as $key=>$partner ) : setup_postdata( $partner ); ?>
+                <div class="vatjss-flex-item-desktop-25">
+                  <a href=<?php echo CFS()->get( 'community_partner_link', $partner->ID ); ?>>
+                  <img src=<?php echo wp_get_attachment_url( get_post_thumbnail_id( $partner->ID ) )  ?> alt=""/>
+                  </a>
+                </div>
+            <?php if( ($key+1)%4 === 0 && (($key+1) != count($partner_posts)) ){ echo '</div><div class="vatjss-flex-container-no-wrap vatjss-hidden-mobile">'; } ?>
+              <?php endforeach; wp_reset_postdata(); ?>
+            </div>
+            <div class="vatjss-flex-container-no-wrap vatjss-hidden-desktop">
+              <?php foreach ( $partner_posts as $key=>$partner ) : setup_postdata( $partner ); ?>
+                <div class="vatjss-flex-item-mobile-50">
+                  <a href=<?php echo CFS()->get( 'community_partner_link', $partner->ID ); ?>>
+                  <img src=<?php echo wp_get_attachment_url( get_post_thumbnail_id( $partner->ID ) )  ?> alt=""/>
+                  </a>
+                </div>
+            <?php if( ($key+1)%2 === 0 && (($key+1) != count($partner_posts)) ){ echo '</div><div class="vatjss-flex-container-no-wrap vatjss-hidden-desktop">'; } ?>
+              <?php endforeach; wp_reset_postdata(); ?>
+            </div>
+          </div>
         </section>
       </article>
 
-			<?php endwhile; // End of the loop. ?>
+			
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
