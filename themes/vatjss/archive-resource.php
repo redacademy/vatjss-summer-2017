e6626a635cd5e09f748640bdadd39f92b0ee8fa8
Camden_Shaw <camden.shaw@gmail.com>
2017-09-12 16:38:34 -0700

    commiting before pull

--- themes/vatjss/archive-resource.php
+++ themes/vatjss/archive-resource.php
@@ -17,15 +17,20 @@ get_header(); ?>
         <div class="community-toggle resource-toggle-mobile"><h3>Community<span> Resources</span></h3></div>
       </div>
       <div class="vatjss-mobile-accordion">
-        <div class="housing-toggle resource-toggle-desktop"><h3>Housing Resources</h3></div>
+        <div class="vatjss-resource-desktop-content">
+          <?php the_title();
+          CFS()->get('subtitle');
+          the_content('293'); ?>
+        </div> 
+        <div class="housing-toggle resource-toggle-desktop"><h3>Housing</h3></div>
         <section class="housing-resource-list resource-list-page">
           <?php get_template_part('template-parts/resources', 'housing'); ?>
         </section>
-        <div class="justice-toggle resource-toggle-desktop"><h3>Justice Resources</h3></div>
+        <div class="justice-toggle resource-toggle-desktop"><h3>Justice</h3></div>
         <section class="justice-resource-list resource-list-page">
           <?php get_template_part('template-parts/resources', 'justice'); ?>
         </section>
-        <div class="community-toggle resource-toggle-desktop"><h3>Community Resources</h3></div>
+        <div class="community-toggle resource-toggle-desktop"><h3>Community</h3></div>
         <section class="community-resource-list resource-list-page">
           <?php get_template_part('template-parts/resources', 'community'); ?>
         </section>
