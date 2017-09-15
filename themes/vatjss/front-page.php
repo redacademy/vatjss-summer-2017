4682fdaaf832ff8d1995e93f9f882af6c4596522
bsoetarto7 <bsoetarto7@gmail.com>
2017-09-15 11:46:03 -0700

    Fix front page

--- themes/vatjss/front-page.php
+++ themes/vatjss/front-page.php
@@ -57,7 +57,7 @@ get_header(); ?>
               <?php
                 foreach ( $service_post as $post ) : setup_postdata( $post );
                   if($post->ID === 113){
-                    echo '<img class="vatjss-home-service-icons vatjss-hidden-mobile" src="'. esc_url(CFS()->get( 'icon' )) .'"/>';
+                    echo '<img class="vatjss-home-service-icons vatjss-hidden-mobile" src="'. CFS()->get( 'icon' ) .'"/>';
                   }
                 endforeach; wp_reset_postdata();
               ?>
@@ -81,7 +81,7 @@ get_header(); ?>
               <?php
                 foreach ( $service_post as $post ) : setup_postdata( $post );
                   if($post->ID === 103){
-                    echo '<img class="vatjss-home-service-icons vatjss-hidden-mobile" src="'. esc_url(CFS()->get( 'icon' )) .'"/>';
+                    echo '<img class="vatjss-home-service-icons vatjss-hidden-mobile" src="'. CFS()->get( 'icon' ) .'"/>';
                   }
                 endforeach; wp_reset_postdata();
               ?>
@@ -90,40 +90,25 @@ get_header(); ?>
               <div class="justice-learn-more-fp"><a href="services/justice-services">Learn More <i class="fa fa-chevron-right" aria-hidden="true"></i></a></div>
             </div>
           </div>
-        </div>
-      </div>
-    </section>
-    <section class="vatjss-home-services-section">
-      <div class="vatjss-container-fluid">
-        <div class="vatjss-flex-container">
           <?php
-            $service_types = get_terms( 'services-type' );
-            $housing = $service_types[1];
-
-            $args_service = array( 'post_type' => 'services');
-            $service_post = get_posts( $args_service );
+            $resources_category = get_categories( 'resources=types' );
+            $resources = $resources_category[0];
           ?>
           <div class="vatjss-flex-item-mobile-100 vatjss-flex-item-desktop-33">
             <div class="vatjss-home-service-mobile-image vatjss-hidden-desktop">
             <?php
               foreach ( $service_post as $post ) : setup_postdata( $post );
-                if($post->ID === 113){
+                if($post->ID === 114){
                   echo the_post_thumbnail( 'full' );
                 }
               endforeach; wp_reset_postdata();
             ?>
             </div>
-            <div class="housing-services-fp">
-              <?php
-                foreach ( $service_post as $post ) : setup_postdata( $post );
-                  if($post->ID === 113){
-                    echo '<img class="vatjss-home-service-icons vatjss-hidden-mobile" src="'. CFS()->get( 'icon' ) .'"/>';
-                  }
-                endforeach; wp_reset_postdata();
-              ?>
-              <h3 class="housing-title-fp">Housing Services</h3>
-              <p class="housing-description-fp vatjss-hidden-desktop"><?php echo $housing->description ?></p>
-              <div class="housing-learn-more-fp"><a href="services/housing-services">Learn More <i class="fa fa-chevron-right" aria-hidden="true"></i></a></div>
+            <div class="resources-fp">
+              <img class="vatjss-home-service-icons vatjss-hidden-mobile" src=<?php echo get_stylesheet_directory_uri(); ?>/images/resources.png />
+              <h3 class="resources-title-fp">Resources</h3>
+              <p class="resources-description-fp vatjss-hidden-desktop"><?php echo $resources->description ?></p>
+              <div class="resources-learn-more-fp"><a href="resource">Learn More <i class="fa fa-chevron-right" aria-hidden="true"></i></a></div>
             </div>
           </div>
           <div class="vatjss-flex-item-mobile-100 vatjss-hidden-desktop">
@@ -132,7 +117,7 @@ get_header(); ?>
             </div>
             <div class="volunteer-fp">
               <h3 class="volunteer-title-fp">Volunteer Opportunities</h3>
-              <p class="volunteer-description-fp"><?php echo esc_html(CFS()->get( 'volunteer_opportunities' )); ?></p>
+              <p class="volunteer-description-fp"><?php echo CFS()->get( 'volunteer_opportunities' ); ?></p>
               <div class="volunteer-learn-more-fp"><a href="contact-us">Learn More <i class="fa fa-chevron-right" aria-hidden="true"></i></a></div>
             </div>
           </div>
@@ -141,7 +126,6 @@ get_header(); ?>
     </section>
     <section class="vatjss-home-mobile-subscribe-banner vatjss-vertical-align-center">
       <button class="subscribe">Subscribe</button>
-      
     </section>
   </main>
 </div>
