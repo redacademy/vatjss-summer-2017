ab8cbd9c605ae75a45ee8e09525645401bb50446
Camden_Shaw <camden.shaw@gmail.com>
2017-09-15 17:30:47 -0700

    fixed issue with functionality not changing with screen size on resource page

--- themes/vatjss/js/resource.js
+++ themes/vatjss/js/resource.js
@@ -14,52 +14,67 @@
       $(this).addClass('resource-accordion-button-toggle-on');
     });
   }
+
   var windowWidth = window.innerWidth;
-  $(window).resize(function() {
+  applyFunctionality(windowWidth);
+
+  $(window).resize(function(windowWidth) {
+    $('.housing-toggle').unbind();
+    $('.community-toggle').unbind();
+    $('.justice-toggle').unbind();
+    $('a[href^="#"]').unbind();
     windowWidth = window.innerWidth;
+    console.log(windowWidth);
+    applyFunctionality(windowWidth);
+    return windowWidth;
   });
-  if ( windowWidth <= 768 ){
 
-    $('.housing-toggle').on('click', function(){
-      $('.resource-toggle-mobile').removeClass('resource-toggle-mobile-on');
-      $('.resource-list-page').removeClass('resource-toggle-on');
-      $('.housing-resource-list').addClass('resource-toggle-on');
-      $('.housing-toggle').addClass('resource-toggle-mobile-on');
-    });
-
-    $('.justice-toggle').on('click', function(){
-      $('.resource-toggle-mobile').removeClass('resource-toggle-mobile-on');
-      $('.resource-list-page').removeClass('resource-toggle-on');
-      $('.justice-resource-list').addClass('resource-toggle-on');
-      $('.justice-toggle').addClass('resource-toggle-mobile-on');
-    });
-
-    $('.community-toggle').on('click', function(){
-      $('.resource-toggle-mobile').removeClass('resource-toggle-mobile-on');
-      $('.resource-list-page').removeClass('resource-toggle-on');
-      $('.community-resource-list').addClass('resource-toggle-on');
-      $('.community-toggle').addClass('resource-toggle-mobile-on');
-    });
-    resourceAccordion();
-  }
-  if ( windowWidth >= 769 ) {
-    $(document).ready(function() {
-      $('a[href^="#"]').on('click', function(event) {
-
-        if (this.hash) {
-          event.preventDefault();
-
-          var hash = this.hash;
-          var focus = $(hash);
-
-          $('html, body').stop().animate({
-            'scrollTop': focus.offset().top - 120
-          }, 400, 'swing', function(){
-            window.location.hash = hash;
-          });
-        }
+  function applyFunctionality(windowW){
+    if ( windowW <= 768 ){
+      $('.housing-toggle').on('click', function(){
+        $('.resource-toggle-mobile').removeClass('resource-toggle-mobile-on');
+        $('.resource-list-page').removeClass('resource-toggle-on');
+        $('.housing-resource-list').addClass('resource-toggle-on');
+        $('.housing-toggle').addClass('resource-toggle-mobile-on');
       });
-    });
+
+      $('.justice-toggle').on('click', function(){
+        $('.resource-toggle-mobile').removeClass('resource-toggle-mobile-on');
+        $('.resource-list-page').removeClass('resource-toggle-on');
+        $('.justice-resource-list').addClass('resource-toggle-on');
+        $('.justice-toggle').addClass('resource-toggle-mobile-on');
+      });
+
+      $('.community-toggle').on('click', function(){
+        $('.resource-toggle-mobile').removeClass('resource-toggle-mobile-on');
+        $('.resource-list-page').removeClass('resource-toggle-on');
+        $('.community-resource-list').addClass('resource-toggle-on');
+        $('.community-toggle').addClass('resource-toggle-mobile-on');
+      });
+      resourceAccordion();
+    }
+    if ( windowW >= 769 ) {
+      if ( windowW < 769 ) {
+        return;
+      }
+      $(document).ready(function() {
+        $('a[href^="#"]').on('click', function(event) {
+
+          if (this.hash) {
+            event.preventDefault();
+
+            var hash = this.hash;
+            var focus = $(hash);
+
+            $('html, body').stop().animate({
+              'scrollTop': focus.offset().top - 120
+            }, 400, 'swing', function(){
+              window.location.hash = hash;
+            });
+          }
+        });
+      });
+    }
     resourceAccordion();
   }
 }(jQuery));
