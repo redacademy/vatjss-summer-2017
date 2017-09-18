0f56f5fb79f16c690bba1d3fffa25ac0fe0dd30d
Chinatsu <chinatsu.k@hotmail.com>
2017-09-18 10:27:25 -0700

    fixed 404 & changed phone # on the header

--- themes/vatjss/404.php
+++ themes/vatjss/404.php
@@ -12,14 +12,15 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 			<section class="error-404">
-
 				<div class="not-found">
-					<h1 class="page-title"><?php echo esc_html( '404' ); ?></h1><span class="error-bug"><i class="fa fa-bug" aria-hidden="true"></i></span>
+					<span class="error-bug"><i class="fa fa-bug" aria-hidden="true"></i></span>
+					<h1 class="page-title"><?php echo esc_html( '404' ); ?></h1>
 				</div>	
 				<div class="page-content">			
 					<p class="vatjss-404-sentence"><?php echo esc_html( 'Sorry!' ); ?></p>
 					<p class="vatjss-404-sentence"><?php echo esc_html( 'There has been a bug in the slug.'); ?></p>
-				  <p class="vatjss-404-sentence"><?php echo esc_html( 'Check URL again.'); ?></p> 
+					<p class="vatjss-404-sentence"><?php echo esc_html( 'This page cannot be found.'); ?></p> 
+					<p class="vatjss-404-sentence"><?php echo esc_html( ' Check URL again.' ); ?></p>
 				</div><!-- .page-content -->
 			</section><!-- .error-404 -->
 
