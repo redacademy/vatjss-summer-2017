2a570828c2cf7e9d403a4b61d644247f31df2219
Camden_Shaw <camden.shaw@gmail.com>
2017-09-17 22:05:31 -0700

    styled contact form pop-up

--- themes/vatjss/js/contact.js
+++ themes/vatjss/js/contact.js
@@ -32,9 +32,12 @@ function openTab(evt, cityName) {
    var wpcf7Elm = document.querySelector( '.wpcf7' );
    
    wpcf7Elm.addEventListener( 'wpcf7mailsent', function( event ) {
-       $('.wpcf7-form').append( '<div class="alert"><h3>Thank You!</h3> <p>Your message has been received. One of the staff from the department you requested service from will be in touch with you</p> <div class="close-alert">X</div></div>');
+       $('.wpcf7-form').append( '<section class="alert-gradient"><div class="alert"><h3>Thank You!</h3> <p>Your message has been received. One of the staff from the department you requested service from will be in touch with you.</p> <div class="close-alert">X</div></div></section>');
        $('.close-alert').on('click', function() {
-           $('.alert').addClass('alert-close');
+            if ( '366' == event.detail.contactFormId ) {
+                $('.alert-gradient').addClass('alert-close');
+                $('.wpcf7-mail-sent-ok').addClass('alert-close');
+            }
        })
    }, false );
 }(jQuery));
